0270c9157f2409f9bf2fb1a4cb4f784287c4302f
ilgreen995-cell <ilgreen995@gmail.com>
2025-10-18 11:11:19 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -241,7 +241,7 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            SELECTING_THEMES: [CallbackQueryHandler(select_theme,per_message=False)],
+            SELECTING_THEMES: [CallbackQueryHandler(select_theme, per_message=False)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
@@ -253,5 +253,6 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
 
 
